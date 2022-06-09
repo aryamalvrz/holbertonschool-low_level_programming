ba79ece5f8720d4c5afd61cafdf99c8dc209f22a
aryamalvrz <aryamalvrz@gmail.com>
2022-06-09 18:55:49 +0000

    PRINTING _putchar FOLLOWED BY A NEW LINE, RETURN 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* main - Printing _putchar followed by a new line.
+* The program should return to 0.
+* Return: 0
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}

ecf54c4438eacbf3a6ec73379fe31ae13f422dc4
aryamalvrz <aryamalvrz@gmail.com>
2022-07-29 22:21:39 +0000

    longest day of my life

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,15 +9,15 @@ void jack_bauer(void)
 	int index;
 	int index2 = 0;
 
-	for(index = 0; index <= 24; index++)
+	for (index = 0; index < 24; index++)
 	{
-		for(index2 = 0; index2 < 60; index2++)
+		for (index2 = 0; index2 < 60; index2++)
 		{
-			_putchar((index / 10)+ '0');
-			_putchar((index % 10)+ '0');
+			_putchar((index / 10) + '0');
+			_putchar((index % 10) + '0');
 			_putchar(':');
-			_putchar((index2 / 10)+ '0');
-			_putchar((index2 % 10)+ '0');
+			_putchar((index2 / 10) + '0');
+			_putchar((index2 % 10) + '0');
 			_putchar('\n');
 		}
 	}

89ca400d473b98dcae59c598402bc7d2233f1e91
aryamalvrz <aryamalvrz@gmail.com>
2022-07-30 00:43:36 +0000

    Prints 9 times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * print_row - prints the rows of the 9 times table
+ * multi_row - number which idx will be multiplied by
+ *
+ * Return: null
+ */
+void print_row(int multi_row)
+{
+	int idx, check;
+
+	for(idx = 0; idx <= 9; idx++)
+	{
+			check = idx * multi_row;
+			if (check > 9)
+			{
+				_putchar(' ');
+				_putchar((check / 10) + '0');
+				_putchar((check % 10) + '0');
+			}
+			else
+			{
+				if (idx != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(check + '0');
+			}
+			if (idx != 9)
+			{
+				_putchar(' ');
+			}
+	}
+}
+
+/**
+ * times_table - prints the columns of the 9 times table
+ *
+ * Return: null
+ */
+void times_table(void)
+{
+	int column;
+
+	for (column = 0; column <= 9; column++)
+	{
+		print_row(column);
+		_putchar('\n');
+	}	
+}

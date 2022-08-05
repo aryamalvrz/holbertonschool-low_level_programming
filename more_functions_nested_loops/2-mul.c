020ace364d89a2b827fab6980ac162c1bd997a2f
aryamalvrz <aryamalvrz@gmail.com>
2022-08-05 01:13:58 +0000

    Multiplication of 2 int on a nested loop

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *mul - Multiplies two int
+ *@a: input character
+ *@b: input character
+ *Return: The result of the multiplication
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}

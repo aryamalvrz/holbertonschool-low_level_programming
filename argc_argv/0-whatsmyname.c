12698d35f662eaf01b5458e682d723cf5c1f5cf8
aryamalvrz <aryamalvrz@gmail.com>
2022-09-01 22:10:17 +0000

    A program that prints its name, followed by a \n

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Prints program name
+ * @argc: Argument Count
+ * @argv: Argument Vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}

cdb0c0468adfd86d2326dd5aa7a93613f45effc6
aryamalvrz <aryamalvrz@gmail.com>
2022-09-17 00:39:07 +0000

    Concatenates two strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,38 +12,38 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, length1 = 0, lenght2 = 0;
-	char *p;
+	unsigned int length1, length2, a, y, sum = 0;
+	char *s;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 
-	lenght1 = _strlen (s1)
-		lenght2 = _strlen (s2)
+	for (length1 = 0; s1[length1] != '\0'; length1++)
 
-		if (n > lenght2)
-			n = lenght2;
-	p = malloc (lenght1 + n + 1);
+		for (length2 = 0; s2[length2] != '\0'; length2++)
 
-	if (p == NULL)
+			if (n >= length2)
+				sum = length1 + length2;
+			else
+				sum = length1 + n;
+	s = malloc(sizeof(char) * (sum + 1));
+
+	if (s == 0)
 		return (NULL);
 
-	for (i = 0; i < lenght1 + n; i++)
+	for (a = 0; s1[a] != '\0'; a++)
 	{
-		if (*s1)
-		{
-			p[i] = *s1
-				s1++;
-		}
-		else
-		{
-			p[i] = *s2;
-			s2++;
-		}
+		s[a] = s1[a];
 	}
-	p[i] = '\0';
-	return (0);
+	if (n < length2)
+		for (y = 0; y < n; y++)
+		{
+			s[a] = s2[y];
+			a++;
+		}
+	s[a] = '\0';
+	return (s);
 }

f3869584717ec687b2070d2e705b3f9c0b7a1612
aryamalvrz <aryamalvrz@gmail.com>
2022-09-27 21:03:43 +0000

    Define a new type of struct on the header

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Creates a structure h named dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Name of the owner
+ */
+
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif

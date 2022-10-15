9259da7b29b9a91d66d47a24ffb6371fb817ad76
aryamalvrz <aryamalvrz@gmail.com>
2022-10-15 03:59:36 +0000

    Header file for Variadic Function Module

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,20 @@
+#ifndef HEADER
+#define HEADER
+
+/*LIBRARIES*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/*PROTOTYPES*/
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_char(va_list ap);
+void print_int(va_list ap);
+void print_float(va_list ap);
+void print_string(va_list ap);
+
+#endif

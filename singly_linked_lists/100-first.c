116f1e4025a863bd9ee92f6075b3ec6ddca62ae9
aryamalvrz <aryamalvrz@gmail.com>
2022-10-26 17:55:00 +0000

    Prints before the main function is executed. Fixed compliation errors.

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -6,8 +6,8 @@
  * Return: 0
  */
 
-void _attribute_((constructor))premain()
+void __attribute__ ((constructor)) premain()
 {
-	printf("You're beat! and yet, you must allow \n
-			I bore my house upon my back! \n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }

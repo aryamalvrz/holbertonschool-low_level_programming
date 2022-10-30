6811586edb3c8a1a5b9ced7cb87d7ba68ed5f581
aryamalvrz <aryamalvrz@gmail.com>
2022-10-30 00:31:27 +0000

    On this commit I will write a function that sets the value of a bit to 0 at a given index

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * set_bit - Function that sets the value of a bit to 1 at a given index
+ *
+ * @n: Pointer for number
+ * @index: Index, starting from 0 of the bit you want to set
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int mask = 1;
+	unsigned int bit = sizeof(n) * 8 - 1;
+
+	if (index > bit)
+		return (-1);
+
+	mask = mask << index;
+
+	*n = *n | mask;
+
+	return (1);
+
+}

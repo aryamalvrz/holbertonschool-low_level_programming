e573b28b22f07cbf2dbe012bde429044b9c58e35
aryamalvrz <aryamalvrz@gmail.com>
2022-11-06 02:03:18 +0000

    With this commit I will write a function that frees a dlistint_t list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Function that frees a dlistint_t list.
+ *
+ * @head: pointer to the first node
+ *
+ * Return: nothing
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+
+}
